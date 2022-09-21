3d76ee7c07131d7e7a5563d09abaa223d24cd1fa
Yunus Emre ÖZTÜRK <50083324+emreyunus61@users.noreply.github.com>
2022-09-21 10:37:37 +0300

    Apache camel route

--- src/main/java/com/example/camelmicroservice/router/c/ActiveMqSenderRouter.java
+++ src/main/java/com/example/camelmicroservice/router/c/ActiveMqSenderRouter.java
@@ -1,4 +1,23 @@
 package com.example.camelmicroservice.router.c;
 
-public class ActiveMqSenderRouter {
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ActiveMqSenderRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        //timer
+        from("timer:active-mq-timer?period=1000")
+                .transform().constant("My message for active mq")
+                .to("activemq:my-activemq-queue");
+          //queue
+    }
+
+
+
+
 }
